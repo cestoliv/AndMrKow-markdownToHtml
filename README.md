f9fb2d9b186e444029a725db19893b4258485fd5
colivier74 <colivier.dev@gmail.com>
2019-08-27 21:02:19 +0200

    first commit!

---
+++ README.md
@@ -0,0 +1,90 @@
+# AndMrKow-markdownToHtml
+(**AndMrKow** is an anagram of **makdown**)
+
+It's a simple tool to translate markdown into html.
+
+------
+## andmrkow.js, to be used with Javascript, NodeJs...
+Javascript version of the tool.
+### Functions :
+**render(text, withSyntaxeElements = false)** *translate markdown into html*
+
+*examples :*
+
+`render("hello world! i'm using **AndMrKow!**")`
+return : `<p>hello world! i'm using <strong>AndMrKow!</strong><p>`
+
+`render("hello world! i'm using **AndMrKow!**", true)`
+return : `<p>hello world! i'm using <strong>**AndMrKow!**</strong></p>`
+
+**whitout = (text, lenght = "all")** *remove markdown syntax elements*
+*examples :*
+
+`without("hello world! i'm using **AndMrKow!**")`
+return : `hello world! i'm using AndMrKow!`
+
+`without("hello world! i'm using **AndMrKow!**", 10)`
+return : `hello worl`
+
+## SUPPORTS
+### Headers
+Use 1-6 hash characters at the start of the line, corresponding to header levels 1-6.
+
+`# This is an H1` >> `<h1>This is an H1</h1>`
+
+`## This is an H2` >> `<h2>This is an H2</h2>`
+
+`###### This is an H6` >> `<h6>This is an H6</h6>`
+
+### Bold
+Surrounding words with two asterisks or two underscores (on each side) will put them in bold.
+
+`Here is a **text in bold**.` >> `<p>Here is a <strong>text in bold</strong>.</p>`
+
+`Here are **two texts** in __bold__.` >> `<p>Here are <strong>two texts</strong> in <strong>bold</strong>.</p>`
+
+### Italics
+Surrounding words with an asterisk or bottom dash (on each side) will put them in italics.
+
+`Here is an *italicized text*.` >> `<p>Here is an <em>italicized text</em>.</p>`
+
+`Here *are two* italic _text_.` >> `<p>Here <em>are two</em> italic <em>text</em>.</p>`
+
+### Code block
+Indenting in front of a line will make a block of code.
+
+`   console.log("hello world!")` >> `<pre><code>console.log("hello world!")</code></pre>`
+
+	if(you like this) {
+    	return(star the project !)
+  	}
+
+will produce :
+
+    <pre><code>if(you like this) {
+        return(star the project !)
+    }</code></pre>
+
+### Inline code
+Surrounding words with `` ` `` turns them into code.
+
+``Small piece of `code`.`` >> `<p>Small piece of <code>code</code>.</p>`
+
+### Links
+There is a special syntax for creating a link: **\[Text\]\(https://chevro.fr\)** and an optional argument **(blank)**, to add a `target =" _ blank "` parameter to the link.
+
+**/!\\**AndMrKow adds **rel="noopener, noreferrer"** parameters to links
+
+`Here is a link to [GitHub](https://github.com).` >> `<p>Here is a link to <a href="https://github.com" rel="noopener, noreferrer">GitHub</a>.</p>`
+
+`Here is a link to [GitHub](https://github.com)(blank) with a target="_blank" parameter.` >> `<p>Here is a link to <a href="https://github.com" rel="noopener, noreferrer" target="_blank">GitHub</a>.</p>`
+
+### Images
+The special syntax of the images is similar to that of the links : **\!\[Description / title\]\(link to the image\)**. 
+Beware of the exclamation mark at the beginning.
+
+`![chevro logo](https://chevro.fr/files/favicon/android-chrome-512x512.png)` >> `<img src="https://chevro.fr/files/favicon/android-chrome-512x512.png" alt="chevro logo" />`
+
+### Comments
+Start a line with a **double slash** that turns it into a comment. It will not be visible in the HTML code.
+`//Here is a cool comment!` >> ""
---
+++ andmrkow.js
@@ -0,0 +1,230 @@
+//AndMrKow-markdownToHtml
+
+render = (text, withSyntaxeElements = false) => { // translate markdown into html
+    var parsedText = "" // will contain all the html text to return
+
+    var lines = text.split(/\r\n|\r|\n/) // create an array of each lines
+
+    //VARS
+    var isCode = false // will be true if we are in a text block
+
+    for(line in lines) { // browse lines (line is the index)
+        var noP = false // will be true if the current line must not be surrounded by <p> tags
+        var isComment = false // will be true if the current line is a comment
+        var regex, found // regex : contain the regex, found : contain results of regex match
+
+    //COMMENT
+        if(/^\/\/(.+)/.exec(lines[line])) { // if the current line start with a double slash
+            if(!withSyntaxeElements) { // if syntaxe elements should not be shown, otherwise we do nothing (we keep the comment).
+                isComment = true // current line is a comment
+                noP = true // current line don't need to be surronded with <p> tags (because she's empty)
+                lines[line] = "" //empty the line
+            }
+        }
+    //CODE BLOCK
+        if(/^\t(.+)/.exec(lines[line])) { // search if current line begins with an indentation
+            if(!isCode) { // if the previous line wasn't a code
+                parsedText += "<pre><code>" // add start code block
+                isCode = true // say that this is the beginning of a block of code
+            }
+            noP = true // code block don't need to be surronded by <p> tags
+            if(withSyntaxeElements) { // if syntaxe elements should be shown
+                lines[line] = "\t" + RegExp.$1 + '\n' // re-add the start indentation
+            }
+            else  {
+                lines[line] = RegExp.$1 + '\n'
+            }
+        }
+        else { // if current line is not a part of code
+            if(isCode) { // if the previous line was a part of code
+                parsedText += "</code></pre>" // close the code block
+                isCode = false // say the current line is not a part of code
+            }
+
+        //INLINE CODE
+            regex = /`(.+?)`/g // search if word/sentence is surronded by `
+            found = lines[line].match(regex) // put matching word/sentence of th current line in the found array
+            for(i in found) { // read found array
+                let data = regex.exec(found[i]) // actualize the regex (otherwise it keep the last matching word/sentence)
+
+                let text = RegExp.$1 // text = the text between the `
+                if(withSyntaxeElements) { // if syntaxe elements should be shown
+                    text = "`" + text + "`" // surrond text by `
+                }
+                lines[line] = lines[line].replace("`" + RegExp.$1 + "`", "<code>" + text + "</code>") // replace old word by the text surronded by <code> tags
+            }
+
+        //BOLD
+            regex = /\*\*(.+?)\*\*/g // search if word/sentence is surronded by **
+            found = lines[line].match(regex) // put matching word/sentence of th current line in the found array
+            for(i in found) { // read found array
+                let data = /\*\*(.+?)\*\*/.exec(found[i]) // actualize the regex (otherwise it keep the last matching word/sentence)
+
+                let text = RegExp.$1 // text = the text between the **
+                if(withSyntaxeElements) { // if syntaxe elements should be shown
+                    text = "**" + text + "**" // surrond text by **
+                }
+                lines[line] = lines[line].replace("**" + RegExp.$1 + "**", "<strong>" + text + "</strong>") // replace old word by the text surronded by <strong> tags
+            }
+            regex = /\_\_(.+?)\_\_/g // search if word/sentence is surronded by __
+            found = lines[line].match(regex) // put matching word/sentence of th current line in the found array
+            for(i in found) { // read found array
+                let data = /\_\_(.+?)\_\_/g.exec(found[i]) // actualize the regex (otherwise it keep the last matching word/sentence)
+
+                let text = RegExp.$1 // text = the text between the __
+                if(withSyntaxeElements) { // if syntaxe elements should be shown
+                    text = "__" + text + "__" // surrond text by __
+                }
+                lines[line] = lines[line].replace("__" + RegExp.$1 + "__", "<strong>" + text + "</strong>") // replace old word by the text surronded by <strong> tags
+            }
+        //EMPHASE
+            regex = /\*(.+?)\*/g // search if word/sentence is surronded by *
+            found = lines[line].match(regex) // put matching word/sentence of th current line in the found array
+            for(i in found) { // read found array
+                let data = /\*(.+?)\*/g.exec(found[i]) // actualize the regex (otherwise it keep the last matching word/sentence)
+
+                let text = RegExp.$1 // text = the text between the *
+                if(withSyntaxeElements) { // if syntaxe elements should be shown
+                    text = "*" + text + "*" // surrond text by *
+                }
+                lines[line] = lines[line].replace("*" + RegExp.$1 + "*", "<em>" + text + "</em>") // replace old word by the text surronded by <em> tags
+            }
+            regex = /\_(.+?)\_/g // search if word/sentence is surronded by _
+            found = lines[line].match(regex) // put matching word/sentence of th current line in the found array
+            for(i in found) { // read found array
+                let data = /\_(.+?)\_/g.exec(found[i]) // actualize the regex (otherwise it keep the last matching word/sentence)
+
+                let text = RegExp.$1 // text = the text between the _
+                if(withSyntaxeElements) { // if syntaxe elements should be shown
+                    text = "_" + text + "_" // surrond text by _
+                }
+                lines[line] = lines[line].replace("_" + RegExp.$1 + "_", "<em>" + text + "</em>") // replace old word by the text surronded by <em> tags
+            }
+        //HEADERS
+            /*h1*/
+            if(/^# (.+)$/.exec(lines[line])) { // search if the current line start with "# "
+                noP = true // headers should not be surronded by <p> tags
+                let text = withSyntaxeElements ? "# " + RegExp.$1 : RegExp.$1 // text = "# + title"  if syntax element should be shown, otherwise, text = title
+                lines[line] = lines[line].replace("# " + RegExp.$1, "<h1>" + text + "</h1>") // replace title in text by a title surronded by <h1> tags
+            }
+            /*h2*/
+            if(/^## (.+)$/.exec(lines[line])) { // search if the current line start with "## "
+                noP = true // headers should not be surronded by <p> tags
+                let text = withSyntaxeElements ? "## " + RegExp.$1 : RegExp.$1 // text = "## + title"  if syntax element should be shown, otherwise, text = title
+                lines[line] = lines[line].replace("## " + RegExp.$1, "<h2>" + text + "</h2>") // replace title in text by a title surronded by <h2> tags
+            }
+            /*h3*/
+            if(/^### (.+)$/.exec(lines[line])) { // search if the current line start with "### "
+                noP = true // headers should not be surronded by <p> tags
+                let text = withSyntaxeElements ? "### " + RegExp.$1 : RegExp.$1 // text = "### + title"  if syntax element should be shown, otherwise, text = title
+                lines[line] = lines[line].replace("### " + RegExp.$1, "<h3>" + text + "</h3>") // replace title in text by a title surronded by <h3> tags   
+            }
+            /*h4*/
+            if(/^#### (.+)$/.exec(lines[line])) { // search if the current line start with "#### "
+                noP = true // headers should not be surronded by <p> tags
+                let text = withSyntaxeElements ? "#### " + RegExp.$1 : RegExp.$1 // text = "#### + title"  if syntax element should be shown, otherwise, text = title
+                lines[line] = lines[line].replace("#### " + RegExp.$1, "<h4>" + text + "</h4>") // replace title in text by a title surronded by <h4> tags
+            }
+            /*h5*/
+            if(/^##### (.+)$/.exec(lines[line])) { // search if the current line start with "##### "
+                noP = true // headers should not be surronded by <p> tags
+                let text = withSyntaxeElements ? "##### " + RegExp.$1 : RegExp.$1 // text = "##### + title"  if syntax element should be shown, otherwise, text = title
+                lines[line] = lines[line].replace("##### " + RegExp.$1, "<h5>" + text + "</h5>") // replace title in text by a title surronded by <h5> tags
+            }
+            /*h6*/
+            if(/^###### (.+)$/.exec(lines[line])) { // search if the current line start with "###### "
+                noP = true // headers should not be surronded by <p> tags
+                let text = withSyntaxeElements ? "###### " + RegExp.$1 : RegExp.$1 // text = "###### + title"  if syntax element should be shown, otherwise, text = title
+                lines[line] = lines[line].replace("###### " + RegExp.$1, "<h6>" + text + "</h6>") // replace title in text by a title surronded by <h6> tags
+            }
+
+            //IMAGES
+            regex = /!\[(.+?)\]\((.+?)\)/g // search if word/sentence respects the pattern ![title/desc](link)
+            found = lines[line].match(regex) // put matching word/sentence of th current line in the found array
+            for(i in found) { // read found array
+                let data = /!\[(.+?)\]\((.+?)\)/g.exec(found[i]) // actualize the regex (otherwise it keep the last matching word/sentence)
+
+                let text = "" // text = the text in "alt" if withSyntaxElements is false, or the syntax element if is true
+                if(withSyntaxeElements) { // if syntaxe elements should be shown
+                    text = "![" + RegExp.$1 + '](<a href="' + htmlspecialchars(RegExp.$2) + '" target="_blank" rel="noopener, noreferrer">' + RegExp.$2 + "</a>)" // re-add the syntax elements
+                }
+                lines[line] = lines[line].replace("![" + RegExp.$1 +"](" + RegExp.$2 + ")", text + '<img src="' + htmlspecialchars(RegExp.$2) + '" alt="' + RegExp.$1 + '" />') // replace old word by the image element
+            }
+
+            //LINK
+            regex = /\[(.+?)\]\((.+?)\)\((.+?)\)|\[(.+?)\]\((.+?)\)/g // search if word/sentence respects the pattern [title/desc](link) or [title/desc](link)(param)
+            found = lines[line].match(regex) // put matching word/sentence of th current line in the found array
+            for(i in found) { // read found array
+                let data = /\[(.+?)\]\((.+?)\)\((.+?)\)|\[(.+?)\]\((.+?)\)/g.exec(found[i]) // actualize the regex (otherwise it keep the last matching word/sentence)
+
+                if(lines[line][lines[line].indexOf(found[i]) - 1] != "!") { // search the caractere juste before the "[title/desc](link)", if it's a !, it's an images
+                    if(RegExp.$3 == "") { // if RegExp.$3 is empty, the "param" section is empty, so it's a link without additionnal param
+                        let text = '<a href="' + htmlspecialchars(RegExp.$5) + '" rel="noopener, noreferrer">' + RegExp.$4 + "</a>" // text = the link
+                        if(withSyntaxeElements) { // if syntaxe elements should be shown
+                            text = "[" + RegExp.$4 + '](<a href="' + htmlspecialchars(RegExp.$5) + '" rel="noopener, noreferrer">' + text + "</a>)" // text = the link with syntax element
+                        }
+                        lines[line] = lines[line].replace("[" + RegExp.$4 +"](" + RegExp.$5 + ")", text) // replace the old word/sentence by the link
+                    }
+                    else {
+                        if(RegExp.$3 == "blank") { // if param is equal to "blank", add target="_blank" to the link
+                            let text = '<a href="' + htmlspecialchars(RegExp.$2) + '" target="_blank" rel="noopener, noreferrer">' + RegExp.$1 + "</a>" // text = the link (with target="_blank")
+                            if(withSyntaxeElements) { // if syntaxe elements should be shown
+                                text = "[" + RegExp.$1 + '](<a href="' + htmlspecialchars(RegExp.$2) + '" target="_blank" rel="noopener, noreferrer">' + RegExp.$1 + "</a>)(" + RegExp.$3 + ")" // text = the link with syntax element (with target="_blank")
+                            }
+                            lines[line] = lines[line].replace("[" + RegExp.$1 +"](" + RegExp.$2 + ")(" + RegExp.$3 + ")", text) // replace the old word/sentence by the link
+                        }
+                        else { // else, no param, same as normal link
+                            let text = '<a href="' + htmlspecialchars(RegExp.$2) + '" rel="noopener, noreferrer">' + RegExp.$1 + "</a>"
+                            if(withSyntaxeElements) { 
+                                text = "[" + RegExp.$1 + '](<a href="' + htmlspecialchars(RegExp.$2) + '" rel="noopener, noreferrer">' + RegExp.$1 + "</a>)(" + RegExp.$3 + ")"
+                            }
+                            lines[line] = lines[line].replace("[" + RegExp.$1 +"](" + RegExp.$2 + ")(" + RegExp.$3 + ")", text)
+                        }
+                    }
+                }
+            }  
+        }
+
+        if((lines[line] == "" || lines[line] == null) && !isComment) { // if line is empty and is not a comment
+            parsedText += "<br />" // add a <br /> tag
+        }
+        else {
+            let parsedLine = lines[line]
+            if(!noP) { // if need to be surronded by <p> tags
+                parsedLine = "<p>" + parsedLine + "</p>" // surrond with <p> tags
+            }
+            parsedText += parsedLine // add line to text to return
+        }    
+    }
+
+    return(parsedText) // return text
+}
+
+htmlspecialchars = (str) => {
+    if (typeof(str) == "string") {
+        str = str.replace(/&/g, "&amp;");
+        str = str.replace(/"/g, "&quot;");
+        str = str.replace(/'/g, "&#039;");
+        str = str.replace(/</g, "&lt;");
+        str = str.replace(/>/g, "&gt;");
+        str = str.replace(/\*/g, "%2A");
+        str = str.replace(/_/g, "%5F");
+    }
+    return str;
+
+    str = str.replace(/;/g, "%3B")
+    str = str.replace(/\//g, "%2F")
+    str = str.replace(/\?/g, "%3F")
+    str = str.replace(/:/g, "%3A")
+    str = str.replace(/@/g, "%40")
+    str = str.replace(/=/g, "%3D")
+    str = str.replace(/&/g, "%26")
+    str = str.replace(/è/g, "%C3%A8")
+    str = str.replace(/é/g, "%C3%A9")
+    str = str.replace(/#/g, "%23")
+}
+
+escapeRegExp = (stringToGoIntoTheRegex) => {
+    console.log(stringToGoIntoTheRegex)
+    return stringToGoIntoTheRegex.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
